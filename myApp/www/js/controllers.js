a80004a7bd351d15dde3b3ea8c521838fcbfd04e
Lawrence Tsao <ltsao2@student.gsu.edu>
2016-02-20 03:40:07 -0500

    lawrence

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -25,6 +25,7 @@ angular.module('starter.controllers', ['ngCordova'])
     $scope.data = {};
 
     $scope.login = function() {
+
       LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
         $state.go('tab.dash');
       }).error(function(data) {
