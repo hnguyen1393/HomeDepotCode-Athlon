f38b03dd828f72beab6b65de98ff22e88453c861
Chin Pyon <ChinPyon@lawn-143-215-53-26.lawn.gatech.edu>
2016-02-19 22:36:13 -0500

    Chin 10:36

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -1,5 +1,24 @@
 angular.module('starter.controllers', ['ngCordova'])
 
+.controller('IntroCtrl', function($scope, $state, $ionicSlideBoxDelegate) {
+
+  // Called to navigate to the main app
+  $scope.startApp = function() {
+    $state.go('main');
+  };
+  $scope.next = function() {
+    $ionicSlideBoxDelegate.next();
+  };
+  $scope.previous = function() {
+    $ionicSlideBoxDelegate.previous();
+  };
+
+  // Called each time the slide changes
+  $scope.slideChanged = function(index) {
+    $scope.slideIndex = index;
+  };
+})
+
 .controller('DashCtrl', function($scope) {})
 
 .controller('ChatsCtrl', function($scope, Chats) {
@@ -16,6 +35,8 @@ angular.module('starter.controllers', ['ngCordova'])
     Chats.remove(chat);
   };
 })
+
+
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
